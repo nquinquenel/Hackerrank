27255bd2c19524a66f99cb70a31cce9ed82e9e05
QUINQUENEL Nicolas <zenntco@gmail.com>
2018-01-28 13:12:07 +0100

    regex specific char

---
+++ Regex/Matching Specific Characters/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Matching Specific Characters
+ * 
+ * @author Nicolas QUINQUENEL
+ */
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[1-3][0-2][xs0][30Aa][xsu][\\.,]$"); // Use \\ instead of using \ 
+    
+    }
+}
