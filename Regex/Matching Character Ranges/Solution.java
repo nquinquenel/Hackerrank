0861ee98b45c76c3e51d5219b033a81d783c4f9b
QUINQUENEL Nicolas <zenntco@gmail.com>
2018-01-28 13:19:06 +0100

    fix com + matching char range

--- Regex/Matching Character Ranges/Solution.java
+++ Regex/Matching Character Ranges/Solution.java
@@ -1,5 +1,5 @@
 /**
- * Excluding Specific Characters
+ * Matching Character Ranges
  * 
  * @author Nicolas QUINQUENEL
  */
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
-        tester.checker("^[^\\d][^aeiou][^bcDF][^\\s][^AEIOU][^\\.,]$"); // Use \\ instead of using \ 
+        tester.checker("^[a-z][1-9][^a-z][^A-Z][A-Z]"); // Use \\ instead of using \ 
     
     }
 }
