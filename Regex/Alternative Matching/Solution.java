b0b8c1cd0971a7ce3ccbb3c337c8d9fe5648f535
QUINQUENEL Nicolas <zenntco@gmail.com>
2018-01-28 13:34:58 +0100

    regex alternative

---
+++ Regex/Alternative Matching/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Alternative Matching
+ * 
+ * @author Nicolas QUINQUENEL
+ */
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^(Mr\\.|Mrs\\.|Ms\\.|Dr\\.|Er\\.)[a-zA-Z]{1,}$"); // Use \\ instead of using \ 
+    
+    }
+}
