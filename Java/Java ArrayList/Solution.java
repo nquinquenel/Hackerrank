69b046cd1be3d56a548509e74d104442d5f444de
root <root@DESKTOP-MF3LNB1.localdomain>
2018-02-10 18:24:36 +0100

    Java ArrayList

---
+++ Java/Java ArrayList/Solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * Java ArrayList
+ * 
+ * @author Nicolas QUINQUENEL
+ */
+
+public class Solution {
+
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        
+        //Add values in the ArrayList
+        int n = sc.nextInt();
+        while (n-- != 0) {
+        	ArrayList<Integer> subList = new ArrayList<Integer>();
+        	int nb = sc.nextInt();
+        	while (nb-- != 0) subList.add(sc.nextInt());
+        	list.add(subList);
+        }
+        
+        //Search for values
+        n = sc.nextInt();
+        while (n-- != 0) {
+        	int x = sc.nextInt();
+        	int y = sc.nextInt();
+        	try {
+                System.out.println(list.get(x - 1).get(y - 1));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("ERROR!");
+            }
+        }
+    }
+}
