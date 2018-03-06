a0fef77580030f13ce8b6efaf66cc5dbae21686f
QUINQUENEL Nicolas <zenntco@gmail.com>
2018-03-07 00:24:40 +0100

    Apple and Orange

---
+++ Algorithms/Implementation/Apple and Orange/Solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * Apple and Orange
+ *
+ * @author Nicolas QUINQUENEL
+ */
+
+public class Solution {
+
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int nbApples = 0;
+        for (int i = 0; i < apples.length; i++) {
+            int applePos = a + apples[i];
+            if (applePos >= s && applePos <= t)
+                nbApples++;
+        }
+        
+        int nbOranges = 0;
+        for (int i = 0; i < oranges.length; i++) {
+            int orangePos = b + oranges[i];
+            if (orangePos >= s && orangePos <= t)
+                nbOranges++;
+        }
+        
+        System.out.println(nbApples + "\n" + nbOranges);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int t = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int[] apple = new int[m];
+        for(int apple_i = 0; apple_i < m; apple_i++){
+            apple[apple_i] = in.nextInt();
+        }
+        int[] orange = new int[n];
+        for(int orange_i = 0; orange_i < n; orange_i++){
+            orange[orange_i] = in.nextInt();
+        }
+        
+        countApplesAndOranges(s, t, a, b, apple, orange);
+        in.close();
+    }
+}
