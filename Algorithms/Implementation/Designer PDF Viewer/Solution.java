f8030c9ee18074f63fe03af816b8a6e8a8ddf099
QUINQUENEL Nicolas <zenntco@gmail.com>
2018-03-08 14:26:13 +0100

    Designer PDF Viewer

---
+++ Algorithms/Implementation/Designer PDF Viewer/Solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ * Designer PDF Viewer
+ *
+ * @author Nicolas QUINQUENEL
+ */
+
+public class Solution {
+
+    static int designerPdfViewer(int[] h, String word) {
+        int highest = 0;
+        String[] tab = word.split("");
+        for (int i = 0; i < tab.length; i++) {
+            if (highest < h[((int) tab[i].charAt(0) - 97)])
+                highest = h[((int) tab[i].charAt(0) - 97)];
+        }
+        return word.length() * highest;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] h = new int[26];
+        for(int h_i = 0; h_i < 26; h_i++){
+            h[h_i] = in.nextInt();
+        }
+        String word = in.next();
+        int result = designerPdfViewer(h, word);
+        System.out.println(result);
+        in.close();
+    }
+}
+
